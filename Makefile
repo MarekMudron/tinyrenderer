77e8bf67dd7f5d676a188932262730d97d65bb4d
Marek Mudron <mudronmarek@gmail.com>
2022-07-06 16:17:10 +0200

    Bresenham

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 SYSCONF_LINK = g++
-CPPFLAGS     =  -ggdb -pg -g -O0
-LDFLAGS      = -ggdb -pg -g -O0
+CPPFLAGS     = 
+LDFLAGS      = 
 LIBS         = -lm
 
 DESTDIR = ./
@@ -11,10 +11,11 @@ OBJECTS := $(patsubst %.cpp,%.o,$(wildcard *.cpp))
 all: $(DESTDIR)$(TARGET)
 
 $(DESTDIR)$(TARGET): $(OBJECTS)
-	$(SYSCONF_LINK) -Wall $(LDFLAGS) -o $(DESTDIR)$(TARGET) $(OBJECTS) $(LIBS)
+	$(SYSCONF_LINK) -Wall $(LDFLAGS)  -o $(DESTDIR)$(TARGET) $(OBJECTS) $(LIBS)
 
 $(OBJECTS): %.o: %.cpp
 	$(SYSCONF_LINK) -Wall $(CPPFLAGS) -c $(CFLAGS) $< -o $@
+
 
 clean:
 	-rm -f $(OBJECTS)
--- main.cpp
+++ main.cpp
@@ -19,15 +19,22 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 		swap(x0, x1);
 		swap(y0, y1);
 	}
+	int dx = x1-x0;
+	int dy = y1-y0;
+	int derror = std::abs(dy)*2;
+	int error = 0;
+	int y = y0;
 	for(int x=x0; x<=x1; x++) {
-		float t = (x - x0)/(float)(x1-x0);
-		int y = y0*(1.-t) + y1*t;
 		if (steep) { 
             image.set(y, x, color); // if transposed, de−transpose 
         } else { 
             image.set(x, y, color); 
         } 
-		
+		error += derror;
+		if (error > .5) {
+			y += (y1 > y0 ? 1 : -1);
+			error -= dx*2;
+		}
 	}
 }
 
