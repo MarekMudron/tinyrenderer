9c081a59f2c443842919210cb00aec54eee7efb8
Marek Mudron <mudronmarek@gmail.com>
2022-07-07 09:58:14 +0200

    Line sweeping

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-
+const TGAColor green   = TGAColor(0, 255,   0,   255);
 
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -38,6 +38,56 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	}
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	if (t0.y>t1.y) std::swap(t0, t1); 
+    if (t0.y>t2.y) std::swap(t0, t2); 
+    if (t1.y>t2.y) std::swap(t1, t2); 
+	int total_height = t2.y - t0.y;
+	for(int y=t0.y; y<= t1.y; y++) {
+		int segmnet_height = t1.y - t0.y + 1;
+		float alpha = (float)(y-t0.y)/total_height;
+		float beta = (float)(y - t0.y)/segmnet_height;
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = t0 + (t1-t0)*beta;
+		if(A.x > B.x) std::swap(A, B);
+		for(int j=A.x; j<=B.x; j++) {
+			image.set(j, y, color);
+		}
+	}
+	for(int y=t1.y; y<= t2.y; y++) {
+		int segmnet_height = t2.y - t1.y + 1;
+		float alpha = (float)(y-t0.y)/total_height;
+		float beta = (float)(y - t1.y)/segmnet_height;
+		Vec2i A = t0 + (t2-t0)*alpha;
+		Vec2i B = t1 + (t2-t1)*beta;
+		if(A.x > B.x) std::swap(A, B);
+		for(int j=A.x; j<=B.x; j++) {
+			image.set(j, y, color);
+		}
+	}
+
+}
+
+int main(int argc, char** argv) {
+	const int width  = 200;
+	const int height = 200;
+    TGAImage image(width, height, TGAImage::RGB);
+
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
+}
+
+/*
 int main(int argc, char** argv) {
 	Model* model;
 	if(argc == 2) {
@@ -67,4 +117,4 @@ int main(int argc, char** argv) {
 	delete model;
 	return 0;
 }
-
+*/
